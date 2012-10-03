cbac9f98ce92789c9be2c475b3e83664eb0170fc
mkrawitz <marc.krawitz@carefusion.com>
2012-10-03 10:56:04 -0500

    Pushing new changes

--- source/app/web/core/IFindCommands.cs
+++ source/app/web/core/IFindCommands.cs
@@ -1,15 +1,32 @@
-﻿namespace app.web.core
+﻿using System.Collections.Generic;
+using System;
+
+namespace app.web.core
 {
   public interface IFindCommands
   {
     IProcessOneRequest get_the_command_that_can_process(IEncapsulateRequestDetails request);
   }
 
-  class CommandRegistry : IFindCommands
+  public class CommandRegistry : IFindCommands
   {
+
+    private IEnumerable<IProcessOneRequest> commands { get; set; }
+
+    public CommandRegistry(IEnumerable<IProcessOneRequest> aCommandList)
+    {
+        commands = aCommandList;
+    }
     public IProcessOneRequest get_the_command_that_can_process(IEncapsulateRequestDetails request)
     {
-      throw new System.NotImplementedException();
+     foreach (IProcessOneRequest processor in commands)
+     {
+         if ( processor.can_handle(request) )
+         {
+             return processor;
+         }
+     }
+        return null;
     }
   }
 }
