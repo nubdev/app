3513f9a9e6739c134a512c1b2328e3eaa2e6615c
Jean-Paul S. Boodhoo <jp@developwithpassion.com>
2012-10-02 19:53:04 -0600

    Updated

--- source/app/Calculator.cs
+++ source/app/Calculator.cs
@@ -1,44 +1,39 @@
 ﻿using System;
 using System.Data;
+using System.Security;
+using System.Security.Principal;
+using System.Threading;
 
 namespace app
 {
-    using System.Security;
-    using System.Security.Principal;
-    using System.Threading;
+  public class Calculator
+  {
+    IDbConnection connection;
 
-    public class Calculator
+    public Calculator(IDbConnection connection)
     {
-        IDbConnection connection;
-
-        public Calculator(IDbConnection connection)
-        {
-            this.connection = connection;
-        }
-
-        public int add(int firstNumber, int secondNumber)
-        {
-            if (firstNumber < 0 ||
-              secondNumber < 0)
-                throw new ArgumentException();
-
-            using (connection)
-            using (var command = connection.CreateCommand())
-            {
-                connection.Open();
-                command.ExecuteNonQuery();
-            }
-            return firstNumber + secondNumber;
-        }
-
-        public void shut_off()
-        {
-            IPrincipal userPrincipal = Thread.CurrentPrincipal;
-            if (!userPrincipal.IsInRole("Admin"))
-            {
-                throw new SecurityException();
-            }
-
-        }
+      this.connection = connection;
     }
+
+    public int add(int firstNumber, int secondNumber)
+    {
+      if (firstNumber < 0 ||
+        secondNumber < 0)
+        throw new ArgumentException();
+
+      using (connection)
+      using (var command = connection.CreateCommand())
+      {
+        connection.Open();
+        command.ExecuteNonQuery();
+      }
+      return firstNumber + secondNumber;
+    }
+
+    public void shut_off()
+    {
+      if (!Thread.CurrentPrincipal.IsInRole("Admin"))
+        throw new SecurityException();
+    }
+  }
 }
