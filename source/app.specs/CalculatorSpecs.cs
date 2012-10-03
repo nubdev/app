9625d8ee9d3480305f1ebe693d88c6d36ccd8b75
Jean-Paul S. Boodhoo <jp@developwithpassion.com>
2012-10-02 18:38:35 -0600

    Updated

--- source/app.specs/CalculatorSpecs.cs
+++ source/app.specs/CalculatorSpecs.cs
@@ -1,15 +1,53 @@
-﻿using Machine.Specifications;
+﻿using System;
+using Machine.Specifications;
+using developwithpassion.specifications.rhinomocks;
+using developwithpassion.specifications.extensions;
 
 namespace app.specs
 {
-
   public class CalculatorSpecs
   {
-    public class when_adding_two_numbers
+    public abstract class concern : Observes<Calculator>
     {
+      
+    }
+
+    public class when_adding_two_numbers : concern
+    {
+      //Arrange
+      Establish c = () =>
+      {
+        
+      };
+
+      //Act
+      Because b = () =>
+        result = sut.add(2, 3);
+
+      //Assert
       It should_return_the_sum = () =>
-        Calculator.add(2, 3).ShouldEqual(5);
-         
-    } 
+        result.ShouldEqual(5);
+
+      static int result;
+    }
+
+    public class when_attempting_to_ADd_a_negative_ToA_p_ositive   : concern
+    {
+      //Arrange
+      Establish c = () =>
+      {
+        
+      };
+
+      //Act
+      Because b = () =>
+        spec.catch_exception(() => sut.add(2, 3));
+
+      //Assert
+      It should_THrow_an_argument_Exception = () =>
+        spec.exception_thrown.ShouldBeAn<ArgumentException>();
+
+      static int result;
+    }
   }
 }
--- source/app/Calculator.cs
+++ source/app/Calculator.cs
@@ -2,7 +2,7 @@
 {
   public class Calculator
   {
-    public static int add(int firstNumber, int secondNumber)
+    public int add(int firstNumber, int secondNumber)
     {
         return firstNumber + secondNumber;
     }
