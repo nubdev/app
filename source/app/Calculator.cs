4b70751cedddd89e588c65ae1e336e4b18b17698
madhatter84gn <madhatter84gn@live.com>
2012-10-02 21:03:11 -0400

    Pushing new changes

--- source/app/Calculator.cs
+++ source/app/Calculator.cs
@@ -2,14 +2,23 @@
 
 namespace app
 {
-  public class Calculator
-  {
-    public int add(int firstNumber, int secondNumber)
+    using System.Data;
+
+    public class Calculator
+    {
+        IDbConnection connection;
+      public Calculator(IDbConnection dbConnection)
+      {
+          connection = dbConnection;
+      }
+
+      public int add(int firstNumber, int secondNumber)
     {
         if(firstNumber < 0 ||
             secondNumber < 0)
             throw new ArgumentException();
 
+          connection.Open();
         return firstNumber + secondNumber;
     }
   }
