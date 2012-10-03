509bda7480ae49d234f2a32b327fa23fa2fe5fbb
Jean-Paul S. Boodhoo <jp@developwithpassion.com>
2012-10-03 09:05:27 -0600

    Updated

---
+++ source/app/web/core/FrontController.cs
@@ -0,0 +1,14 @@
+﻿namespace app.web.core
+{
+  public interface IProcessRequests
+  {
+    void handle(object request);
+  }
+  public class FrontController : IProcessRequests
+  {
+    public void handle(object request)
+    {
+      throw new System.NotImplementedException();
+    }
+  }
+}
