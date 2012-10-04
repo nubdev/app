f6d59efc556a83fccfa6afc3f92d5cc31d943760
Jean-Paul S. Boodhoo <jp@developwithpassion.com>
2012-10-04 17:10:52 -0600

    Updated

--- source/app/core/containers/DependencyFactories.cs
+++ source/app/core/containers/DependencyFactories.cs
@@ -1,7 +1,12 @@
-﻿namespace app.core.containers
+﻿using System;
+
+namespace app.core.containers
 {
   public class DependencyFactories : IFindDependencyFactories
   {
-     
+    public ICreateOneDependency get_the_factory_that_can_create(Type dependency)
+    {
+      throw new NotImplementedException();
+    }
   }
 }
