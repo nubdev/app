fa5ba53dd863ba6d71623372eab2e9145c3ed041
Matt Ellis <matt.ellis.civ@gmail.com>
2012-10-04 11:34:53 -0500

    Pushing new changes

---
+++ source/app.specs/AuthenticatorSpecs.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Machine.Specifications;
+using app.web.application.catalogbrowsing;
+using app.web.core;
+using System.Security.Permissions;
+using developwithpassion.specifications.rhinomocks;
+
+namespace app.specs
+{
+    [Subject(typeof(Authenticator))]
+    class AuthenticatorSpecs
+    {
+        //Write a test to authenticate a user
+        //Test authentication code
+        //test passing authentication to viewing reports
+
+        //Query: Will only users with certain roles be allowed to view certain data
+        //Query: Will I need to use a PrinciplePermission object
+
+        public abstract class concern : Observes<IValidateAUser<>,
+                                      ViewAReport<ViewAReportSpecs.AReport>>
+        {
+        }
+
+   
+    }
+}
---
+++ source/app/web/application/catalogbrowsing/Authenticator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace app.web.application.catalogbrowsing
+{
+    public class Authenticator
+    {
+
+
+    }
+}
