597f1d19e7313a48a0eaa4bd56ddc1a8848a5a9a
mkrawitz <marc.krawitz@carefusion.com>
2012-10-04 17:42:20 -0500

    Pushing new changes

--- source/app/core/containers/basic/Container.cs
+++ source/app/core/containers/basic/Container.cs
@@ -5,17 +5,27 @@ namespace app.core.containers.basic
   public class Container:  IFetchDependencies
   {
     IFindDependencyFactories factories;
+    private ItemCreationFailure_Behaviour failureBehavior;
 
-    public Container(IFindDependencyFactories factories)
+    public Container(IFindDependencyFactories factories, ItemCreationFailure_Behaviour failureBehavior)
     {
       this.factories = factories;
+      this.failureBehavior = failureBehavior;
     }
 
     public Dependency an<Dependency>()
     {
-      var factory = factories.get_the_factory_that_can_create(typeof(Dependency));
-      var item = factory.create();
-      return (Dependency)item;
+        Dependency item = default(Dependency);
+        try
+        {
+            var factory = factories.get_the_factory_that_can_create(typeof (Dependency));
+            item = (Dependency)(factory.create());
+        }
+        catch (Exception e)
+        {
+            throw failureBehavior.Invoke(typeof (Dependency), e);
+        }
+        return (Dependency)item;
     }
   }
 }
